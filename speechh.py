794fc0b38e7a3a1e29feb323270f01ceec54e099
bilal20480 <bilalboxer2005@gmail.com>
2025-05-25 20:26:33 +0530

    Update speechh.py

--- speechh.py
+++ speechh.py
@@ -8,9 +8,10 @@ import nltk
 import google.generativeai as genai
 import pandas as pd
 from datetime import datetime
+api_key=st.secrets["bilal_api"]
 
 # Configure Gemini API key
-genai.configure(api_key="AIzaSyBqx7s51Swc_l8jJILSjWjqyeNYvJXnFj0")  # 🔁 Replace with your actual API key
+genai.configure(api_key=api_key)  # 🔁 Replace with your actual API key
 
 # Download necessary NLTK resources
 nltk.download("punkt")
